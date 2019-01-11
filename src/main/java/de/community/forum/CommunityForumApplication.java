fe965e8592731d8eedc688f1358e55d6fae9d9fe
Régis Martin <martin-regis@web.de>
2019-01-11 11:07:01 +0100

    Added Spring Framework to Project

---
+++ src/main/java/de/community/forum/CommunityForumApplication.java
@@ -0,0 +1,14 @@
+package de.community.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityForumApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
